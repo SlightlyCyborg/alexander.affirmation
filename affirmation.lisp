a11a180def42a3d9171f50638dcf0eb83dedc155
slightlycyborg <collinalexbell@gmail.com>
2018-02-12 21:52:23 -0600

    fun schedule-affirmation added

--- affirmation.lisp
+++ affirmation.lisp
@@ -15,3 +15,15 @@
 
 (defun print-affirmation (instance)
   (format t "~%Ponder this affirmation:~%~a~%~%" (.content instance)))
+
+(defun schedule-affirmation (index sec min hour)
+  (labels ((play-sound ()
+             (inferior-shell:run/nil '(afplay "/Users/taggart/Downloads/light.mp3")))
+           (reminder ()
+             (format t "~%REMEMBER ~a~%" (.content (nth index affirmations)))
+             (sb-thread:make-thread #'play-sound)))
+        (schedule-today #'reminder sec min hour)))
+
+
+;;;;TODO Implement function that adds & prints nested todos?
+(defun add-dependency ())
