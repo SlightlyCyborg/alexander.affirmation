a695a7522c6e3bce7f4ac1cddeec56e34a675833
slightlycyborg <collinalexbell@gmail.com>
2018-03-19 01:24:19 -0600

    (schedule affirmation now speaks to me)

--- affirmation.lisp
+++ affirmation.lisp
@@ -19,9 +19,11 @@
 (defun say-affirmation (instance)
   (say (format nil "~a" (.content instance))))
 
-(defun schedule-affirmation (index sec min hour)
+(defun schedule-affirmation (index sec min hour &optional (sound-not-voice nil))
   (labels ((play-sound ()
-             (inferior-shell:run/nil '(afplay "/Users/taggart/Downloads/light.mp3")))
+             (if sound-not-voice
+                 (inferior-shell:run/nil '(afplay "/Users/taggart/Downloads/light.mp3"))
+                 (say-affirmation (nth index affirmations))))
            (reminder ()
              (format t "~%REMEMBER ~a~%" (.content (nth index affirmations)))
              (sb-thread:make-thread #'play-sound)))
